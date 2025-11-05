949c8833e20fc782762cee0313503bbaaf4751bb
Flo G <floragu2004@gmail.com>
2025-11-05 16:21:50 -0500

    Update papers-data.js

--- papers-data.js
+++ papers-data.js
@@ -1,11 +1,12 @@
 window.papersDocuments = [
   {
-    title: "Sample Google Doc",
-    id: "REPLACE_WITH_GOOGLE_DOC_ID",
+    title: "Does verb-preposition dependence modulate sluicing acceptability in English, and its implications?",
+    id: "1XJ74uSLC-F_iZ-gyMMiWRoaXrV18KZ3iRG-Fbo2jhG0",
     type: "gdoc",
   },
   {
-    title: "Sample PDF or DOCX",
-    id: "REPLACE_WITH_ANOTHER_FILE_ID",
+    title: "Review: The definite DP island in wh-questions and relative clauses",
+    id: "1RDmKZE2VVqdxT2PUA8vwbGUwc7NNb8nB3feQqPwCqNg",
+    type: "gdoc",
   },
 ];
